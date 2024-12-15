c3077624807c00eb0e561c7fde37e6b8b1656ba4
NewDawn0 <newdawn.v0.0+git@gmail.com>
2024-12-15 19:01:30 +0100

    update flake

--- flake.nix
+++ flake.nix
@@ -14,29 +14,33 @@
       packages = eachSystem (system:
         let pkgs = mkPkgs system;
         in {
-          default = pkgs.stdenv.mkDerivation {
+          default = pkgs.gccStdenv.mkDerivation {
             pname = "up-core";
-            name = "up";
             version = "1.0.0";
-            src = ./.;
-            buildInputs = [ pkgs.gcc pkgs.gnumake ];
-            buildPhase = "make build";
+            src = pkgs.fetchFromGitHub {
+              owner = "NewDawn0";
+              repo = "up";
+              rev = "1be487a84782bc62593326c35160fa782e2e7d7a";
+              hash = "sha256-Ll6gHuWh2s7ke9Vqlw0H3tdKHIuvIC13TGHHKllEQ54=";
+            };
             installPhase = ''
-              mkdir -p $out/bin $out/lib
-              cp up-core $out/bin
-              echo "#!/usr/bin/env bash" > $out/lib/SOURCE_ME.sh
+              install -D up-core -t $out/bin
+              mkdir -p $out/lib
+              echo "#!/${pkgs.runtimeShell}" > $out/lib/SOURCE_ME.sh
               $out/bin/up-core --init >> $out/lib/SOURCE_ME.sh
-              chmod +x $out/lib/SOURCE_ME.sh
             '';
             shellHook = ''
               source $out/lib/SOURCE_ME.sh
             '';
-            meta = with pkgs.lib; {
-              description = "A fast way to go up relative directories";
+            meta = {
+              description = "A tool to navigate up directories more swiftly";
+              longDescription = ''
+                This utility allows you to move up relative directories with ease, enhancing efficiency when working in nested file systems. It simplifies navigation with fewer keystrokes.
+              '';
               homepage = "https://github.com/NewDawn0/up";
-              license = licenses.mit;
-              maintainers = [ NewDawn0 ];
-              platforms = platforms.all;
+              license = pkgs.lib.licenses.mit;
+              maintainers = with pkgs.lib.maintainers; [ NewDawn0 ];
+              platmors = pkgs.lib.platforms.all;
             };
           };
         });
