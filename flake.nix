7e18c12116cf91fcf95cd6578e7e271dc58a16b5
NewDawn0 <newdawn.v0.0+git@gmail.com>
2024-12-15 19:04:42 +0100

    update flake

--- flake.nix
+++ flake.nix
@@ -17,12 +17,7 @@
           default = pkgs.gccStdenv.mkDerivation {
             pname = "up-core";
             version = "1.0.0";
-            src = pkgs.fetchFromGitHub {
-              owner = "NewDawn0";
-              repo = "up";
-              rev = "1be487a84782bc62593326c35160fa782e2e7d7a";
-              hash = "sha256-Ll6gHuWh2s7ke9Vqlw0H3tdKHIuvIC13TGHHKllEQ54=";
-            };
+            src = ./.;
             installPhase = ''
               install -D up-core -t $out/bin
               mkdir -p $out/lib
