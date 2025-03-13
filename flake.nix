55c7ad98d522e3c5a882548cdd2ea84332ca3b8d
NewDawn0 <newdawn.v0.0+git@gmail.com>
2025-03-13 15:43:23 +0100

    use nixUtils

--- flake.nix
+++ flake.nix
@@ -1,43 +1,37 @@
 {
   description = "A utility to cd up directories faster";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
-    nix-systems.url = "github:nix-systems/default";
-  };
+  inputs.utils.url = "github:NewDawn0/nixUtils";
 
-  outputs = { nixpkgs, ... }@inputs:
-    let
-      eachSystem = nixpkgs.lib.genAttrs (import inputs.nix-systems);
-      mkPkgs = system: nixpkgs.legacyPackages.${system};
-    in {
-      packages = eachSystem (system:
-        let pkgs = mkPkgs system;
-        in {
-          default = pkgs.gccStdenv.mkDerivation {
-            pname = "up-core";
-            version = "1.0.0";
-            src = ./.;
-            installPhase = ''
-              install -D up-core -t $out/bin
-              mkdir -p $out/lib
-              echo "#!/${pkgs.runtimeShell}" > $out/lib/SOURCE_ME.sh
-              $out/bin/up-core --init >> $out/lib/SOURCE_ME.sh
-            '';
-            shellHook = ''
-              source $out/lib/SOURCE_ME.sh
-            '';
-            meta = {
-              description = "A tool to navigate up directories more swiftly";
-              longDescription = ''
-                This utility allows you to move up relative directories with ease, enhancing efficiency when working in nested file systems. It simplifies navigation with fewer keystrokes.
-              '';
-              homepage = "https://github.com/NewDawn0/up";
-              license = pkgs.lib.licenses.mit;
-              maintainers = with pkgs.lib.maintainers; [ NewDawn0 ];
-              platmors = pkgs.lib.platforms.all;
-            };
-          };
-        });
+  outputs = { self, utils, ... }: {
+    overlays.default = final: prev: {
+      up = self.packages.${prev.system}.default;
     };
+    packages = utils.lib.eachSystem { } (pkgs: {
+      default = pkgs.stdenv.mkDerivation {
+        pname = "up-core";
+        version = "1.0.0";
+        src = ./.;
+        installPhase = ''
+          install -D up-core -t $out/bin
+          mkdir -p $out/lib
+          echo "#!/${pkgs.runtimeShell}" > $out/lib/SOURCE_ME.sh
+          $out/bin/up-core --init >> $out/lib/SOURCE_ME.sh
+        '';
+        shellHook = ''
+          source $out/lib/SOURCE_ME.sh
+        '';
+        meta = {
+          description = "A tool to navigate up directories more swiftly";
+          longDescription = ''
+            This utility allows you to move up relative directories with ease, enhancing efficiency when working in nested file systems. It simplifies navigation with fewer keystrokes.
+          '';
+          homepage = "https://github.com/NewDawn0/up";
+          license = pkgs.lib.licenses.mit;
+          maintainers = with pkgs.lib.maintainers; [ NewDawn0 ];
+          platmors = pkgs.lib.platforms.all;
+        };
+      };
+    });
+  };
 }
